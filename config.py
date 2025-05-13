a68376603aaaa2b2cc5e6b60ffa1bd884333dbbd
Rakesh220696 <109289818+Rakesh220696@users.noreply.github.com>
2025-05-13 15:40:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-BOT_TOKEN = "7563791022:AAHYB75jPrlhEmMkkKd2tXf57XoZXGemaAg"
-ADMIN_ID = 5325245091  # Replace with your Telegram user ID
+
+import os
+
+BOT_TOKEN = os.getenv("7563791022:AAHYB75jPrlhEmMkkKd2tXf57XoZXGemaAg")
+ADMIN_ID = int(os.getenv("5325245091", "0"))
